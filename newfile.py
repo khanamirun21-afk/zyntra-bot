b980850fb41799fa11a24054c091434b5685b8b3
khanamirun21-afk <khanamirun21@gmail.com>
2026-07-25 12:12:11 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,21 +1,20 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# Token Render Environment Variable se aayega
 TOKEN = os.environ["TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🚀 Welcome to Zyntra!")
 
-def main():
+async def main():
     app = Application.builder().token(TOKEN).build()
-
-    # /start command
     app.add_handler(CommandHandler("start", start))
 
     print("✅ Zyntra Bot Started...")
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    

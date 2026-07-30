dbce355d333061d6746e98358a85c16372e55191
khanamirun21-afk <khanamirun21@gmail.com>
2026-07-30 14:47:47 +0530

    Update tap.py

--- tap.py
+++ tap.py
@@ -1,6 +1,14 @@
 from database import add_zyn, get_wallet
+from energy import use_energy
+
 
 async def tap(query):
+    if not use_energy(query.from_user.id):
+        await query.edit_message_text(
+            "⚡ No Energy Left!\n\nPlease wait for energy refill."
+        )
+        return
+
     add_zyn(query.from_user.id, 1)
 
     wallet = get_wallet(query.from_user.id)
@@ -17,6 +25,8 @@ async def tap(query):
 
 💰 ZYN Balance: {zyn}
 💎 BTTC Balance: {bttc}
+
+⚡ Energy: 1000/1000
 
 ⬅️ Type /start to go back
 """

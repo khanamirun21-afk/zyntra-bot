2ebed8721a07b976274b2eba010c83bc79792d74
khanamirun21-afk <khanamirun21@gmail.com>
2026-07-30 14:44:53 +0530

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,1 +1,11 @@
+MAX_ENERGY = 1000
 
+
+def get_energy(user_id):
+    # Abhi temporary energy
+    return MAX_ENERGY
+
+
+def use_energy(user_id):
+    # Abhi hamesha 1 energy use hogi
+    return True

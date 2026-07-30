d4f22b8c75bf98d7aa8a06c430d6ecec5287daab
khanamirun21-afk <khanamirun21@gmail.com>
2026-07-30 13:36:24 +0530

    Update tap.py

--- tap.py
+++ tap.py
@@ -1,1 +1,14 @@
+from database import add_zyn, get_wallet
 
+
+def tap(user_id):
+    add_zyn(user_id, 1)
+
+    wallet = get_wallet(user_id)
+
+    if wallet:
+        zyn, bttc = wallet
+    else:
+        zyn, bttc = (0, 0)
+
+    return zyn, bttc
